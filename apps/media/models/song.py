de8826da0648646ee9f63140c3f4d3b7b7e2888c
kkkirill <kkg.410@mail.ru>
2020-04-04 10:05:06 +0300

    added like functionality, tests

--- apps/media/models/song.py
+++ apps/media/models/song.py
@@ -1,18 +1,25 @@
-from django.db.models import (BooleanField, CharField, FileField, ImageField,
-                              ManyToManyField, Model, PositiveIntegerField)
+from django.contrib.contenttypes.fields import GenericRelation
+from django.db.models import (
+    BooleanField, CharField, ManyToManyField, Model, PositiveIntegerField,
+    URLField)
 
-from utils.utils import generate_upload_path
+from apps.likes.models.like import Like
 
 
 class Song(Model):
     title = CharField(max_length=200)
     duration = PositiveIntegerField(default=0)
-    image = ImageField(default=None, upload_to='media/')
-    file = FileField(upload_to=generate_upload_path, default=None)
+    image = URLField(default='image.png')
+    file = URLField()
     listens = PositiveIntegerField(default=0)
-    explicit = BooleanField()
+    explicit = BooleanField(default=False)
     artists = ManyToManyField('Artist', related_name='songs')
     genres = ManyToManyField('Genre', related_name='songs')
+    likes = GenericRelation(Like, related_query_name='songs')
 
     def __str__(self):
         return self.title
+
+    @property
+    def total_likes(self):
+        return self.likes.count()
